20a09c7d8b062e386f7360619ba5f4777ff1ac52
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-01-30 15:17:32 +0700

    Color

--- src/components/Editor/BasicEditor.js
+++ src/components/Editor/BasicEditor.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
+import classNames from 'classnames';
 import { Button, Tooltip, Popover, Dropdown, Menu, Icon } from 'antd';
-import { CompositeDecorator, EditorState, RichUtils, KeyBindingUtil, getDefaultKeyBinding } from 'draft-js';
+import { Modifier, EditorState, RichUtils, KeyBindingUtil, getDefaultKeyBinding } from 'draft-js';
 import { customStyleMap, customColorMap } from '@/config/constants';
 import Editor from 'draft-js-plugins-editor';
 import styles from './BasicEditor.less';
@@ -9,6 +10,8 @@ import styles from './BasicEditor.less';
 const { hasCommandModifier } = KeyBindingUtil;
 
 const BasicEditor = ({ editorState, onChange, placeholder }) => {
+    const [colorVisible, setColorVisible] = useState(false);
+
     const handleKeyCommand = command => {
         if (command === 'highlight') {
             return onChange(RichUtils.toggleInlineStyle(editorState, 'HIGHLIGHT'));
@@ -30,6 +33,57 @@ const BasicEditor = ({ editorState, onChange, placeholder }) => {
         e.preventDefault();
         onChange(RichUtils.toggleInlineStyle(editorState, inlineStyle));
     };
+    const handleToggleColor = color => e => {
+		e.preventDefault();
+		const selection = editorState.getSelection();
+
+		// Let's just allow one color at a time. Turn off all active colors.
+		const nextContentState = _.keys(customColorMap)
+		.reduce((contentState, color) => {
+			return Modifier.removeInlineStyle(contentState, selection, color)
+		}, editorState.getCurrentContent());
+
+		let nextEditorState = EditorState.push(
+			editorState,
+			nextContentState,
+			'change-inline-style'
+		);
+
+		const currentStyle = editorState.getCurrentInlineStyle();
+
+		// Unset style override for current color.
+		if (selection.isCollapsed()) {
+			nextEditorState = currentStyle.reduce((state, color) => {
+				return !!customColorMap[color] ? RichUtils.toggleInlineStyle(state, color) : state;
+			}, nextEditorState);
+		}
+
+		// If the color is being toggled on, apply it.
+		if (!currentStyle.has(color)) {
+			nextEditorState = RichUtils.toggleInlineStyle(
+				nextEditorState,
+				color
+			);
+		}
+
+		onChange(nextEditorState);
+		setColorVisible(false);
+    };
+    const customColorMapKeys = _.keys(customColorMap);
+	let colorData = _.map(customColorMapKeys, colorKey => ({
+		key: colorKey,
+		...customColorMap[colorKey]
+	}));
+	colorData = _.chunk(colorData, 5);
+	const currentInlineStyle = editorState.getCurrentInlineStyle();
+	let activeKey;
+	for (let i = 0; i < customColorMapKeys.length; ++i) {
+		if (currentInlineStyle.has(customColorMapKeys[i])) {
+			activeKey = customColorMapKeys[i];
+			break;
+		}
+	}
+	if (!activeKey) activeKey = 'BLACK';
     return (
         <div className={styles.basicEditor}>
             <div className={styles.buttons}>
@@ -45,6 +99,35 @@ const BasicEditor = ({ editorState, onChange, placeholder }) => {
                 <Tooltip placement="top" title="Highlight | Cmd+H">
                     <span className={styles.btn} onMouseDown={handleInlineStyle('HIGHLIGHT')}><Icon type="highlight" /></span>
                 </Tooltip>
+                <Popover
+                    placement="bottom"
+                    content={(
+                        <div>
+                            <div className={styles.header}>Select color</div>
+                            <div className={styles.colors}>
+                                {_.map(colorData, colorRow => (
+                                    <div key={_.uniqueId('color_row_')} className={styles.row}>
+                                        {_.map(colorRow, color => (
+                                            <span 
+                                                className={activeKey === color.key ? classNames(styles.active, styles.color) : styles.color}
+                                                key={color.key}
+                                                onMouseDown={handleToggleColor(color.key)}
+                                            >
+                                                <span className={styles.icon} style={{ backgroundColor: color.color }}/>
+                                            </span>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                    popupClassName={styles.colorPopover}
+                    trigger="hover"
+                    visible={colorVisible}
+                    onVisibleChange={setColorVisible}
+                >
+                    <span className={styles.btn} ><Icon type="font-colors" /></span>
+                </Popover>
             </div>
             <div className={styles.editor}>
                 <Editor
