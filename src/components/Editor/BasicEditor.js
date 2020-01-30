aa2ad62a7472ebbf25da92df901e42029b89ab84
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-01-30 15:03:44 +0700

    handle key command

--- src/components/Editor/BasicEditor.js
+++ src/components/Editor/BasicEditor.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
 import { Button, Tooltip, Popover, Dropdown, Menu, Icon } from 'antd';
-import { CompositeDecorator, EditorState, RichUtils } from 'draft-js';
+import { CompositeDecorator, EditorState, RichUtils, KeyBindingUtil, getDefaultKeyBinding } from 'draft-js';
 import { customStyleMap, customColorMap } from '@/config/constants';
 import Editor from 'draft-js-plugins-editor';
 import styles from './BasicEditor.less';
 
+const { hasCommandModifier } = KeyBindingUtil;
+
 const BasicEditor = ({ editorState, onChange, placeholder }) => {
+    const handleKeyCommand = command => {
+        if (command === 'highlight') {
+            return onChange(RichUtils.toggleInlineStyle(editorState, 'HIGHLIGHT'));
+        }
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    };
+    const keyBindingFn = e => {
+        if (e.keyCode === 72 && hasCommandModifier(e)) {
+            return 'highlight';
+        }
+        return getDefaultKeyBinding(e);
+    }
     const handleInlineStyle = inlineStyle => e => {
         e.preventDefault();
         onChange(RichUtils.toggleInlineStyle(editorState, inlineStyle));
     };
-
     return (
         <div className={styles.basicEditor}>
             <div className={styles.buttons}>
@@ -34,6 +52,8 @@ const BasicEditor = ({ editorState, onChange, placeholder }) => {
                     editorState={editorState}
                     onChange={onChange}
                     placeholder={placeholder}
+                    handleKeyCommand={handleKeyCommand}
+                    keyBindingFn={keyBindingFn}
                 />
             </div>
         </div>
