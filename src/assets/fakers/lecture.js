4b6a67a6ec32356302ae66de4cf50dc69d12715f
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-02-02 16:05:13 +0700

    lecture UI

---
+++ src/assets/fakers/lecture.js
@@ -0,0 +1,71 @@
+export default {
+    _id: 1,
+    title: 'Understand What Analytics data to Collect (Tip 1)',
+    time: 12,
+    createdAt: 1578813445900,
+    updatedAt: 1578813445900,
+    chapter: {
+        _id: 1,
+        title: 'The Vue Router'
+    },
+    content: `<div><p role="heading" aria-level="5"><strong>A “Small” Definition of Big Data </strong></p><p>The term ‘big
+    data’ seems to be popping up everywhere these days. And there seems to be as
+    many uses of this term as there are contexts in which you find it: ‘big data’
+    is often used to refer to any dataset that is difficult to manage using
+    traditional database systems; it is also used as a catch-all term for any
+    collection of data that is too large to process on a single server; yet others
+    use the term to simply mean “a lot of data”; sometimes it turns out it doesn’t
+    even have to be large. So what exactly is big data?</p><p>A precise
+    specification of ‘big’ is elusive. What is considered big for one organization
+    may be small for another. What is large-scale today will likely seem
+    small-scale in the near future; petabyte is the new terabyte. Thus, size alone
+    cannot specify big data. The complexity of the data is an important factor that
+    must also be considered.</p><p>Most now agree
+    with the characterization of big data using the 3 V’s coined by Doug Laney of
+    Gartner: </p><p>·      
+    Volume: This refers
+    to the vast amounts of data that is generated every second/minute/hour/day in
+    our digitized world. </p><p>·      
+    Velocity: This refers
+    to the speed at which data is being generated and the pace at which data moves
+    from one point to the next. </p><p>·      
+    Variety: This refers
+    to the ever-increasing different forms that data can come in, e.g., text,
+    images, voice, geospatial. </p><p>A fourth V is
+    now also sometimes added:</p><p>·      
+    Veracity: This refers
+    to the quality of the data, which can vary greatly.</p><p>There are many other V's that gets added to these depending on the context. For our specialization, we will add:</p><p>·       Valence: This refers
+    to how big data can bond with each other, forming connections between otherwise disparate datasets. </p><p>The above V’s
+    are the dimensions that characterize big data, and also embody its challenges:
+    We have huge amounts of data, in different formats and varying quality, that
+    must be processed quickly. </p><p>It is
+    important to note that the goal of processing big data is to gain insight to
+    support decision-making. It is not sufficient to just be able to capture and
+    store the data. The point of collecting and processing volumes of complex data
+    is to understand trends, uncover hidden patterns, detect anomalies, etc. so
+    that you have a better understanding of the problem being analyzed and can make
+    more informed, data-driven decisions. In fact, many consider value as the sixth
+    V of big data:</p><p>·      
+    Value: Processing big
+    data must bring about value from insights gained.</p><p>To address the
+    challenges of big data, innovative technologies are needed. Parallel,
+    distributed computing paradigms, scalable machine learning algorithms, and
+    real-time querying are key to analysis of big data. Distributed file systems,
+    computing clusters, cloud computing, and data stores supporting data variety
+    and agility are also necessary to provide the infrastructure for processing of
+    big data. <a href="http://words.sdsc.edu/words-data-science/workflows" target="_blank" rel="noopener nofollow">Workflows</a> provide an
+    intuitive, reusable, scalable and reproducible way to process big data to gain
+    verifiable value from it in and enable application of same methods to different
+    datasets.</p><p>With all the
+    data generated from social media, smart sensors, satellites, surveillance
+    cameras, the Internet, and countless other devices, big data is all around us.
+    The endeavor to make sense out of that data brings about exciting opportunities
+    indeed!</p><p>Source:
+    <a href="http://words.sdsc.edu/words-data-science/big-data" target="_blank" rel="noopener nofollow">http://words.sdsc.edu/words-data-science/big-data</a> 
+    
+    
+    
+    </p></div>`,
+    nextLectureId: 6,
+    prevLectureId: 3
+};
--- src/pages/Learning/Lecture.js
+++ src/pages/Learning/Lecture.js
@@ -1,24 +1,173 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
-import Link from 'umi/Link';
-import { Skeleton, Icon, Modal, Spin } from 'antd';
+import router from 'umi/router';
+import { EditorState } from 'draft-js';
+import Editor from '@/components/editor/ImageEditor';
+import { Skeleton, Icon, Modal, Spin, Divider, Button, Form, Input, message } from 'antd';
+import LECTURE from '@/assets/fakers/lecture';
+import { exportToHTML } from '@/utils/editor';
+import { minutesToHour } from '@/utils/utils';
 import styles from './Lecture.less';
 
-const Lecture = () => {
+const ButtonGroup = Button.Group;
+const FormItem = Form.Item;
+
+const Lecture = ({ match }) => {
     const [lecture, setLecture] = useState(null);
     const [initLoading, setInitLoading] = useState(false);
+    const [questionVisible, setQuestionVisible] = useState(false);
+    const [questionTitle, setQuestionTitle] = useState({
+        value: '',
+        validateStatus: 'success',
+        help: ''
+    });
+    const [questionContent, setQuestionContent] = useState(EditorState.createEmpty());
+    useEffect(() => {
+        const { lectureId } = match.params;
+        message.success(`Fetch lecture with id ${lectureId}`);
+        setInitLoading(true);
+        setTimeout(() => {
+            setLecture(LECTURE);
+            setInitLoading(false);
+        }, 2000);
+    }, [match.params.lectureId]);
+    const handleCompleteLecture = () => {
+        const { lectureId } = match.params;
+        message.success(`Complete lecture with id ${lectureId}`);
+    };
+    const handleAskQuestion = () => setQuestionVisible(true);
+    const goToLecture = lectureId => {
+        const { courseId } = match.params;
+        router.push(`/learning/${courseId}/lecture/${lectureId}`);
+    };
+    const handleChangeQuestionTitle = e => {
+        const val = e.target.value;
+        if (!val || val.length === '') {
+            setQuestionTitle({
+                validateStatus: 'error',
+                help: 'You must enter title',
+                value: val
+            });
+        }
+        else setQuestionTitle({
+            validateStatus: 'success',
+            help: '',
+            value: val
+        });
+    };
+    const handleCancelAskQuestion = () => {
+        setQuestionTitle({
+            validateStatus: 'success',
+            help: '',
+            value: ''
+        });
+        setQuestionContent(EditorState.createEmpty());
+        setQuestionVisible(false);
+    };
+    const handleSubmitQuestion = () => {
+        if (questionTitle.value === '') return message.error('You must enter title!');
+        else {
+            const contentState = questionContent.getCurrentContent();
+            if (!contentState.hasText()) return message.error('You must enter question!');
+        };
+        //do something
+        //call exportToHTML
+        handleCancelAskQuestion();
+    };
     return (
         <div className={styles.lecture}>
             {!lecture || initLoading ? (
                 <div className={styles.loading}>
                     <Skeleton className={styles.titleSkeleton} active title={null} paragraph={{ rows: 1, width: '96%' }} />
-                    <Skeleton active title={null} paragraph={{ rows: 1, width: '42%' }} />
+                    <Skeleton active title={null} paragraph={{ rows: 2, width: ['62%', '42%'] }} />
                     <div className={styles.spin}>
                         <Spin indicator={<Icon type="loading" style={{ fontSize: 64 }} spin />} />
                     </div>
                 </div>
             ) : (
-                <div></div>
+                <React.Fragment>
+                    <div className={styles.main}>
+                        <div className={styles.title}>
+                            {lecture.title}
+                        </div>
+                        <div className={styles.chapter}>
+                            {`Chapter ${lecture.chapter.title}`}
+                        </div>
+                        <div className={styles.extra}>
+                            <span className={styles.updatedAt}>
+                                {(lecture.updatedAt === lecture.createdAt ? `Created on ${moment(lecture.updatedAt).format('MM/YYYY')}` : `Last updated on ${moment(lecture.updatedAt).format('MM/YYYY')}`)}
+                            </span>
+                            <span className={styles.time}>
+                                {`${minutesToHour(lecture.time)} read.`}
+                            </span>
+                        </div>
+                        <div className={styles.content}>
+                            <div dangerouslySetInnerHTML={{ __html: lecture.content }} />
+                        </div>
+                    </div>
+                    <Divider dashed className={styles.divider} />
+                    <div className={styles.options}>
+                        <ButtonGroup>
+                            {lecture.prevLectureId && (
+                                <Button  onClick={() => goToLecture(lecture.prevLectureId)}>
+                                    <Icon type="left" />
+                                    Prev
+                                </Button>
+                            )}
+                            <Button  onClick={handleAskQuestion}>
+                                Ask a question
+                            </Button>
+                            <Button  onClick={handleCompleteLecture}>
+                                Completed
+                            </Button>
+                            {lecture.nextLectureId && (
+                                <Button  onClick={() => goToLecture(lecture.nextLectureId)}>
+                                    Next
+                                    <Icon type="right" />
+                                </Button>
+                            )}
+                        </ButtonGroup>
+                    </div>
+                    <Modal
+                        className={styles.newQuestionModal}
+                        title={<div className={styles.modalTitle}>Ask a new question</div>}
+                        width={600}
+                        centered
+                        okText="Submit"
+                        visible={questionVisible}
+                        onOk={handleSubmitQuestion}
+                        onCancel={handleCancelAskQuestion}
+                        bodyStyle={{ padding: '35px 10px' }}
+                    >
+                        <Form>
+                            <FormItem label="Title" help={questionTitle.help} validateStatus={questionTitle.validateStatus} required>
+                                <Input 
+                                    type="text"
+                                    placeholder="Title"
+                                    value={questionTitle.value}
+                                    onChange={handleChangeQuestionTitle}
+                                    style={{ width: '100%' }}
+                                />
+                            </FormItem>
+                            <div className={styles.questionLectureTitle}>
+                                <span>Lecture:</span>
+                            </div>
+                            <div className={styles.questionLectureWrapper}>
+                                {`${lecture.title} (${lecture.chapter.title})`}
+                            </div>
+                            <div className={styles.questionContentTitle}>
+                                <span>Content:</span>
+                            </div>
+                            <div className={styles.questionContentWrapper}>
+                                <Editor
+                                    editorState={questionContent}
+                                    onChange={editorState => setQuestionContent(editorState)}
+                                    placeholder="Enter question..."
+                                />
+                            </div>
+                        </Form>
+                    </Modal>
+                </React.Fragment>
             )}
         </div>
     )
